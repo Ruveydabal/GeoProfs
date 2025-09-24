717428d3e8b617d4fdb13e9ff9bf78510c3ffbf3
Roverfloats <tygoliebers@gmail.com>
2025-09-24 15:53:28 +0200

    einde dag

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Voorpagina from "./pages/Voorpagina"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={</>} /> */}
+        <Route path="/" element={<Voorpagina/>} />
       </Routes>
     </BrowserRouter>
   );
