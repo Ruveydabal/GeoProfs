f0d29f07b555c1f8ea5ca8e5e9a4f74efc823f92
Roverfloats <tygoliebers@gmail.com>
2025-09-30 11:07:59 +0200

    header op voorpagina gezet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import './App.css'
 
+import Voorpagina from './pages/Voorpagina'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Voorpagina />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Voorpagina.jsx
+++ src/pages/Voorpagina.jsx
@@ -1,6 +1,10 @@
+import Header from '../components/Header'
+
 function Voorpagina() {
   return (
-    <></>
+    <>
+      <Header/>
+    </>
   )
 }
 
