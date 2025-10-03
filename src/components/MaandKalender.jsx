e46dfd374f14044bada2e9a26943abf6c8619278
Roverfloats <tygoliebers@gmail.com>
2025-10-03 14:06:31 +0200

    einde dag

--- src/components/MaandKalender.jsx
+++ src/components/MaandKalender.jsx
@@ -3,6 +3,9 @@ import moment from 'moment';
 function MaandKalender({ maand, jaar}) {
     //https://ajshah7.medium.com/simple-calendar-using-reactjs-momentjs-861737c6cc8c
 
+    //tijdelijke variabelen
+    var managerRol = true;
+
     // checkt of een datum deel is van de geselecteerde maand
     // niet in maand = true
     const DagNietInMaand = (week, datum) => {
@@ -40,7 +43,17 @@ function MaandKalender({ maand, jaar}) {
                         {/* map dagen in week array */}
                         {week.map((dag) => (
                             <td key={dag} className={`flex h-full w-[calc(100%/7)] border-1 border-solid border-[#D0D0D0] ${DagNietInMaand(index, dag) ? 'bg-[#D0D0D0]' : 'bg-[#fff]'}`}>
-                                <div className='w-full h-full'>{dag}</div>
+                                <div className='w-full h-full'>
+                                    <div className='flex w-full h-[40px]'>
+                                        <div className='flex h-full w-[40px] justify-center items-center'>{dag}</div>
+                                        <div className='flex h-full flex-1 justify-center items-center'>{/*fetch hier nummer aantal afwezig op een dag*/}</div>
+                                    </div>
+                                    {managerRol ? 
+                                    <div className='w-full'></div>
+                                    :
+                                    <></>
+                                    }
+                                </div>
                             </td>
                         ))}
                     </tr>
