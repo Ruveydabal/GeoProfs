6a96659657ebaf6a6dfbe62e3bfe640f7ed2bd06
Ruveydabal <ruveydabal024@gmail.com>
2025-10-21 11:56:55 +0200

    Update App
    Je kan nu bij pagina Ziekmelden.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
+        <Route path="/Ziekmelden" element={<Ziekmelden/>} />
       </Routes>
     </BrowserRouter>
   );
