c9b22b194e0c7c4afea6d5f930d1d69372b39f03
Roverfloats <tygoliebers@gmail.com>
2025-11-05 11:23:04 +0100

    audit pagina is vrijwel klaar, gwn een paar kleine failsafes

--- src/components/AuditTabel.jsx
+++ src/components/AuditTabel.jsx
@@ -1,147 +1,178 @@
 import { db } from "../firebase";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
 import { useState, useEffect } from 'react';
 import moment from 'moment';
 
 function AuditTabel() {
-
     const [auditTrailData, SetAuditTrailData] = useState([])
+    const [ladenOfFaalText, SetLadenOfFaalText] = useState("Aan het laden...")
 
     useEffect(() => {
-        const FetchLogs = async () => {
-            //fetch Audits
+        const FetchAudits = async () => {
             var auditTrails = await getDocs(
-                query(collection(db, "auditTrail"))
+                //orderBy hier zorgt ervoor dat nieuwere logs boven staan, maar haalt dan logs waar datum mist niet op.
+                query(collection(db, "auditTrail"), orderBy('laatstGeupdate', 'desc'))
             );
-            if (auditTrails.empty) 
-                throw new Error("1 niet gevonden.");
+            if (auditTrails.empty) {
+                SetLadenOfFaalText("Er is een fout opgetreden.")
+                throw new Error("Geen Audits Gevonden");
+            }
 
             const auditTrailsdata = auditTrails.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 
             SetAuditTrailData(auditTrailsdata);
         }
-        FetchLogs()
+        FetchAudits()
     }, []);
 
     if(!auditTrailData || auditTrailData.length == 0){
-        return("loading")
+        return(ladenOfFaalText)
     }
+    return (
+        <div className="flex w-full h-auto overflow-auto">
+            <table className="h-auto border-collapse">
+                <thead className="h-[120px]">
+                    <tr>
+                        <td className="min-w-[300px] h-full">
+                            <div className="h-[80px]">
+                                <input
+                                    className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
+                                <p>Actie</p>
+                            </div>
+                        </td>
+                        <td className="min-w-[300px] h-full">
+                            <div className="h-[80px]">
+                                <input
+                                    className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
+                                <p>Uitgevoerd op tabel</p>
+                            </div>
+                        </td>
+                        <td className="min-w-[300px] h-full">
+                            <div className="h-[80px]">
+                                <input
+                                    className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
+                                <p>Uitgevoerd door</p>
+                            </div>
+                        </td>
+                        <td className="min-w-[300px] h-full">
+                            <div className="h-[80px]">
+                                <input
+                                    className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
+                                <p>Uitgevoerd op</p>
+                            </div>
+                        </td>
+                        <td className="min-w-[300px] h-full">
+                            <div className="h-[80px]">
+                                <input
+                                    className="flex justify-center items-center w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
+                                <p>Type uitvoering</p>
+                            </div>
+                        </td>
+                        <td className="min-w-[300px] h-full">
+                            <div className="h-[80px]">
+                                <input
+                                    className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
+                                <p>Datum en tijd</p>
+                            </div>
+                        </td>
+                    </tr>
+                </thead>
 
-  return (
-    <div className="flex w-full h-auto overflow-auto">
-        <table className="h-auto border-collapse">
-            <thead className="h-[120px]">
-                <tr>
-                    <td className="min-w-[300px] h-full">
-                        <div className="h-[80px]">
-                            <input
-                                className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
-                                type="text"
-                            />
-                        </div>
-                        <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
-                            <p>Actie</p>
-                        </div>
-                    </td>
-                    <td className="min-w-[300px] h-full">
-                        <div className="h-[80px]">
-                            <input
-                                className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
-                                type="text"
-                            />
-                        </div>
-                        <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
-                            <p>Uitgevoerd op tabel</p>
-                        </div>
-                    </td>
-                    <td className="min-w-[300px] h-full">
-                        <div className="h-[80px]">
-                            <input
-                                className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
-                                type="text"
-                            />
-                        </div>
-                        <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
-                            <p>Uitgevoerd door</p>
-                        </div>
-                    </td>
-                    <td className="min-w-[300px] h-full">
-                        <div className="h-[80px]">
-                            <input
-                                className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
-                                type="text"
-                            />
-                        </div>
-                        <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
-                            <p>Uitgevoerd op</p>
-                        </div>
-                    </td>
-                    <td className="min-w-[300px] h-full">
-                        <div className="h-[80px]">
-                            <input
-                                className="flex justify-center items-center w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
-                                type="text"
-                            />
-                        </div>
-                        <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
-                            <p>Type uitvoering</p>
-                        </div>
-                    </td>
-                    <td className="min-w-[300px] h-full">
-                        <div className="h-[80px]">
-                            <input
-                                className="w-[250px] h-[40px] bg-[#F4F4F4] rounded-[15px] border border-[#D0D0D0]"
-                                type="text"
-                            />
-                        </div>
-                        <div className="flex justify-center items-center h-[40px] border border-[#D0D0D0]">
-                            <p>Datum en tijd</p>
-                        </div>
-                    </td>
-                </tr>
-            </thead>
+                <tbody>
+                    {
+                        auditTrailData.map((audit, i) => (
+                            <tr key={i} className={`h-[40px] ${i % 2 == 0 ? 'bg-[#DDE7F1]' : 'bg-[#fff]'}`}>
+                                <td className="min-w-[300px] border border-[#D0D0D0]">
+                                    <p className="pl-[10px]">
+                                        {
+                                            audit?.actie && audit?.actie.titel ?
+                                            audit?.actie.titel :
+                                            ""
+                                        }
+                                    </p>
+                                </td>
+                                <td className="min-w-[300px] border border-[#D0D0D0]">
+                                    <p className="pl-[10px]">
+                                        {
+                                            audit?.tabel && audit?.tabel.tabelNaam ?
+                                            audit?.tabel.tabelNaam :
+                                            ""
+                                        }
+                                    </p>
+                                </td>
+                                <td className="min-w-[300px] border border-[#D0D0D0]">
+                                    <p className="pl-[10px]">
+                                        {
+                                            audit?.uitgevoerdDoorUser && audit?.uitgevoerdDoorUser.naam && audit?.uitgevoerdDoorUser.achternaam ?
+                                            audit?.uitgevoerdDoorUser.naam + " " + audit?.uitgevoerdDoorUser.achternaam :
+                                            ""
+                                        }
+                                    </p>
+                                </td>
+                                <td className="min-w-[300px] border border-[#D0D0D0]">
+                                    <p className="pl-[10px]">
+                                        {
+                                            //check of veld bestaat
+                                            !audit?.uitgevoerdOp ? 
+                                            "" :
 
-            <tbody>
-                {
-                    auditTrailData.map(audit => (
-                        <tr key={audit.id} className={`h-[40px] ${audit.id % 2 == 0 ? 'bg-[#fff]' : 'bg-[#DDE7F1]'}`}>
-                            {console.log(audit)}
-                            <td className="min-w-[300px] border border-[#D0D0D0]">
-                                <p className="pl-[10px]">
-                                    {audit?.actie.titel}
-                                </p>
-                            </td>
-                            <td className="min-w-[300px] border border-[#D0D0D0]">
-                                <p className="pl-[10px]">{audit?.tabel.tabelNaam}</p>
-                            </td>
-                            <td className="min-w-[300px] border border-[#D0D0D0]">
-                                <p className="pl-[10px]">{audit?.uitgevoerdDoorUser.naam + " " + audit?.uitgevoerdDoorUser.achternaam}</p>
-                            </td>
-                            <td className="min-w-[300px] border border-[#D0D0D0]">
-                                <p className="pl-[10px]">{
-                                    audit?.uitgevoerdOp.tabel.tabelNaam == "user" ?
-                                    audit?.uitgevoerdOp.naam :
-                                    audit?.uitgevoerdOp.tabel.tabelNaam == "verlof" ?
-                                    "verlof id: " + audit?.uitgevoerdOp.id :
-                                    ""
-                                    }</p>
-                            </td>
-                            <td className="min-w-[300px] border border-[#D0D0D0]">
-                                <p className="pl-[10px]">
-                                    {audit?.typeUitvoering == undefined ?
-                                    "" : audit?.typeUitvoering.titel}
-                                </p>
-                            </td>
-                            <td className="min-w-[300px] border border-[#D0D0D0]">
-                                <p className="pl-[10px]">{moment(audit?.laatstGeupdate.toDate()).format("DD-MM-YYYY HH:mm:ss")}</p>
-                            </td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
-    </div>
-  );
+                                            //check of tabel user is, dan gebruik naam
+                                            audit?.uitgevoerdOp.tabel.tabelNaam == "user" ?
+                                            audit?.uitgevoerdOp.naam :
+
+                                            //anders gebruik tabel en id
+                                            audit?.uitgevoerdOp.tabel.tabelNaam + " id: " + audit?.uitgevoerdOp.id
+                                        }
+                                    </p>
+                                </td>
+                                <td className="min-w-[300px] border border-[#D0D0D0]">
+                                    <p className="pl-[10px]">
+                                        {
+                                            !audit?.typeUitvoering ?
+                                            "" : audit?.typeUitvoering.titel
+                                        }
+                                    </p>
+                                </td>
+                                <td className="min-w-[300px] border border-[#D0D0D0]">
+                                    <p className="pl-[10px]">
+                                        {
+                                            !audit?.laatstGeupdate ?
+                                            "" :                                    
+                                            moment(audit?.laatstGeupdate.toDate()).format("DD-MM-YYYY HH:mm:ss")
+                                        }
+                                    </p>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default AuditTabel
