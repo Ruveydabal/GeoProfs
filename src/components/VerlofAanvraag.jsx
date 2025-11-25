d483ab58a027fe38eadce0422dfaeb755a4702bd
Roverfloats <tygoliebers@gmail.com>
2025-11-25 11:25:06 +0100

    database verbinding

--- src/components/VerlofAanvraag.jsx
+++ src/components/VerlofAanvraag.jsx
@@ -2,39 +2,48 @@ import moment from 'moment';
 import Checkbox from './basis-components/CheckBox';
 import { useState } from 'react';
 
-function VerlofAanvraag({typeKaart, id, naam, reden, startDatum, eindDatum, status}) {
-        const [multiselectGechecked, SetMultiselectGechecked] = useState(false)
+function VerlofAanvraag({verlofData, typeKaart, userData, verlofStatusData}) {
+    const [multiselectGechecked, SetMultiselectGechecked] = useState(false)
 
-  return (
-    <div className="w-full h-auto border-1 border-solid border-[#D0D0D0] p-[20px] divide-y divide-[#D0D0D0]">
-        <div className="w-full h-auto pb-[20px]">
-            <p className='text-xl'>{naam}</p>
-            <p className='my-[5px]'>{reden}</p>
-            <p>{`Van ${moment(startDatum).format("DD-MM-YYYY")}, tot ${moment(eindDatum).format("DD-MM-YYYY")}`}</p>
+    if (!verlofData || !typeKaart || !verlofStatusData){
+        "kaart kon niet laden."
+        return
+    }
+
+    var userData = userData.filter(x => x.id == verlofData.user_id.id)[0]
+    var status = verlofStatusData.filter(x => x.id == verlofData.statusVerlof_id.id)[0]
+
+    if (status.id != 1 && status.id != 2){
+        return
+    }
+
+    return (
+        <div className="w-full h-auto border-1 border-solid border-[#D0D0D0] p-[20px] divide-y divide-[#D0D0D0] mb-[20px]">
+            <div className="w-full h-auto pb-[20px]">
+                <p className='text-xl'>{`${userData.voornaam} ${userData.achternaam}`}</p>
+                <p className='my-[5px]'>{verlofData.omschrijvingRedenVerlof}</p>
+                <p>{`Van ${moment(verlofData.startDatum.toDate()).format("DD-MM-YYYY")}, tot ${moment(verlofData.eindDatum.toDate()).format("DD-MM-YYYY")}`}</p>
+            </div>
+            <div className="w-full h-auto pt-[20px]">
+                {
+                    typeKaart == "geschiedenis" ?
+                        <p>{status.omschrijving}</p> :
+                    typeKaart == "openAanvragen" ?
+                        <button className='h-[40px] max-w-[90%] w-[200px] bg-[#2AAFF2] text-white rounded-[15px] cursor-pointer' onClick={() => {}}>Aanvraag annuleren</button> :
+                    typeKaart == "manager" ? 
+                        <div className='flex w-full justify-between'>
+                            <div className='w-auto'>
+                                <button className='h-[40px] max-w-[90%] w-[120px] bg-[#00BC00] text-white rounded-[15px] cursor-pointer mr-[20px]' onClick={() => {}}>Goedkeuren</button>
+                                <button className='h-[40px] max-w-[90%] w-[120px] bg-[#DF121B] text-white rounded-[15px] cursor-pointer' onClick={() => {}}>Afkeuren</button>
+                            </div>
+
+                            <Checkbox id={id} onChange={(e) => SetMultiselectGechecked(e.target.checked)}/>
+                        </div> : 
+                    "kaart kon niet laden"
+                    
+                }
+            </div>
         </div>
-        <div className="w-full h-auto pt-[20px]">
-            {
-                typeKaart == "geschiedenis" ?
-                    <p>{status}</p> :
-                typeKaart == "openAanvragen" ?
-                    <button className='h-[40px] max-w-[90%] w-[200px] bg-[#2AAFF2] text-white rounded-[15px] cursor-pointer' onClick={() => {}}>Aanvraag annuleren</button> :
-                typeKaart == "manager" ? 
-                    <div className=''>
-                        <>
-                            <button className='h-[40px] max-w-[90%] w-[120px] bg-[#00BC00] text-white rounded-[15px] cursor-pointer mr-[20px]' onClick={() => {}}>Goedkeuren</button>
-                            <button className='h-[40px] max-w-[90%] w-[120px] bg-[#DF121B] text-white rounded-[15px] cursor-pointer' onClick={() => {}}>Afkeuren</button>
-                        </>
-
-                        <Checkbox id={id} onChange={(e) => SetMultiselectGechecked(e.target.checked)}/>
-
-                        
-                        {/* <input type="checkbox" className='appearance-none cursor-pointer w-[20px] h-[20px] border-1 border-solid border-[#D0D0D0] rounded-[5px]' /> */}
-                    </div> : 
-                "kaart kon niet laden"
-                
-            }
-        </div>
-    </div>
-  );
+    );
 }
 export default VerlofAanvraag;
--- src/pages/Verlofoverzicht.jsx
+++ src/pages/Verlofoverzicht.jsx
@@ -1,33 +1,91 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { db } from "../firebase";
+import { useState, useEffect } from 'react';
+import { collection, query, getDocs } from "firebase/firestore";
 import Header from "../components/Header.jsx";
 import VerlofAanvraag from "../components/VerlofAanvraag.jsx"
 
 import moment from 'moment';
 
 function Verlofoverzicht() {
-      let navigate = useNavigate();
+  let navigate = useNavigate();
 
+    const [verlofData, SetVerlofData] = useState([])
+    const [userData, SetUserData] = useState([])
+    const [verlofStatusData, SetVerlofStatusData] = useState([])
+    const [ladenOfFaalText, SetLadenOfFaalText] = useState("Aan het laden...")
+
+    useEffect(() => {
+      const FetchVerlofAanvragen = async () => {
+        try {
+        const verlofSnap = await getDocs(
+          query(collection(db, "verlof"))
+        );
+
+        const userSnap = await getDocs(
+          query(collection(db, "user"))
+        );
+
+        const verlofStatusSnap = await getDocs(
+          query(collection(db, "statusVerlof"))
+        );
+
+        if (verlofSnap.empty || userSnap.empty) {
+          SetLadenOfFaalText("Er is geen verlof gevonden.");
+          SetVerlofData([]);
+          return;
+        }
+
+        const verlofData = verlofSnap.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+
+        const userData = userSnap.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+
+        const verlofStatusData = verlofStatusSnap.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+
+        SetVerlofData(verlofData);
+        SetUserData(userData);
+        SetVerlofStatusData(verlofStatusData);
+        } catch (err) {
+          console.error("Fout bij het ophalen van verlof aanvragen:", err);
+          SetLadenOfFaalText("Er is iets misgegaan bij het ophalen van de verlof aanvragen.");
+        }
+      };
+
+      FetchVerlofAanvragen();
+    }, []);
+    
   return (
     <>
       <Header />
       <div className="h-[90%] w-full">
             <div className='h-[120px] w-full flex items-center justify-between px-[50px]'>
                 <button className='h-[40px] max-w-[90%] w-[100px] bg-[#2AAFF2] text-white rounded-[15px] cursor-pointer' onClick={() => {navigate("/voorpagina")}}>Home</button>
-                <button className='h-[40px] max-w-[90%] w-[200px] bg-[#2AAFF2] text-white rounded-[15px] cursor-pointer' onClick={{}}>Verlof aanvragen</button>
+                <button className='h-[40px] max-w-[90%] w-[200px] bg-[#2AAFF2] text-white rounded-[15px] cursor-pointer' onClick={() => {navigate("/")}}>Verlof aanvragen</button>
             </div>
-            
-            <div className='flex h-[calc(100%-120px)] w-full divide-x divide-[#D0D0D0] px-[40px]'>
-              <div className="h-full flex-1 px-[10px]">
-
-                <VerlofAanvraag typeKaart={"geschiedenis"} id={1} naam={"test"} reden={"test"} startDatum={moment()} eindDatum={moment()} status={"goedgekeurd"}/>
+            {
+              !verlofData || verlofData.length == 0 ? 
+                ladenOfFaalText :
+              <div className='flex h-[calc(100%-120px)] w-full divide-x divide-[#D0D0D0] px-[40px]'>
+                <div className="h-full flex-1 px-[10px] overflow-y-scroll">
+                  {verlofData.map((verlof) => (
+                    <VerlofAanvraag key={verlof.id} verlofData={verlof} typeKaart={"geschiedenis"} userData={userData} verlofStatusData={verlofStatusData}/>
+                  ))}
+                </div>
+                <div className="h-full flex-1 px-[10px]">
+                </div>
+                <div className="h-full flex-1 px-[10px]">
+                </div>
               </div>
-              <div className="h-full flex-1 px-[10px]">
-                <VerlofAanvraag typeKaart={"openAanvragen"} id={2} naam={"test"} reden={"test"} startDatum={moment()} eindDatum={moment()} status={"goedgekeurd"}/>
-              </div>
-              <div className="h-full flex-1 px-[10px]">
-                <VerlofAanvraag typeKaart={"manager"} id={2} naam={"test"} reden={"test"} startDatum={moment()} eindDatum={moment()} status={"goedgekeurd"}/>
-              </div>
-            </div>
+            }
       </div>
     </>
     
