f816fedecaad9095864cee224bd8e8c4e4286eb4
anke180 <153827509+anke180@users.noreply.github.com>
2025-11-26 14:33:41 +0100

    update profile home knop

--- src/pages/Profiel.jsx
+++ src/pages/Profiel.jsx
@@ -104,11 +104,10 @@ function Profiel() {
 
     return (
         <>
-            <Header />
             <div className='h-[90%] w-full'>
                 <div className='h-[120px] w-full flex items-center'>
                     <button className='h-[40px] max-w-[90%] w-[100px] bg-[#2AAFF2] text-white rounded-[15px] ml-[50px] cursor-pointer'
-                        onClick={() => navigate("/voorpagina")}>Home</button>
+                        onClick={() => navigate(`/${gebruiker?.rol?.toLowerCase().replaceAll(" ", "")}/voorpagina`)}>Home</button>
                 </div>
                 <div className='flex h-[calc(100%-120px)] flex-1 ml-[50px]'>
                     <div className='h-full w-[300px]'>
