31444033596ce2ee629c8a7a03e05c518156fb1d
Roverfloats <roverfloats@gmail.com>
2025-12-02 10:38:52 +0100

    popup styling

--- src/components/VerlofAfkeurenPopup.jsx
+++ src/components/VerlofAfkeurenPopup.jsx
@@ -14,12 +14,20 @@ function VerlofAfkeurenPopup({SetPopupWeergeven, verlofData}) {
     <div className="flex justify-center items-center absolute top-0 left- 0 w-full h-full bg-[#00000050]">
         <div className="w-[500px] p-[50px] h-auto bg-[#fff] rounded-[15px]">
             <div className="flex w-full h-full items-center flex-col">
-                <p>{verlofData.id}</p>
+                <p className='w-full text-center text-[25px] mb-[20px]'>Verlof Afkeuren</p>
+                <p className='w-full text-center'>Wat is de reden van afkeuring?</p>
                 <textarea name="" id=""
+                  className='w-full h-[200px] resize-none mb-[20px] border-1 border-solid border-[#D0D0D0] rounded-[15px] bg-[#F4F4F4] p-[10px]'
                   value={afkeurReden}
+                  placeholder="test"
                   onChange={e => {setAfkeurReden(e.target.value), console.log(afkeurReden)}}
-                ></textarea>
-                
+
+                />
+                <div className='w-full flex justify-between'>
+                  <button className='h-[40px] w-[150px] bg-[#fff] rounded-[15px] border-1 border-solid border-[#D0D0D0] cursor-pointer' onClick={console.log("test 1")}>Annuleren</button>
+                  <button className='h-[40px] w-[150px] bg-[#2AAFF2] text-white rounded-[15px] cursor-pointer' onClick={console.log("test 2")}>Bevestigen</button>
+                  
+                </div>
             </div>
         </div>
     </div>
