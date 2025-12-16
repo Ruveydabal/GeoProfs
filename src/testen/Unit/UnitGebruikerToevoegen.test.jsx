1a4fa6019dc2f6aa273c5eb73aaf0ea5713c9e38
anke180 <153827509+anke180@users.noreply.github.com>
2025-12-16 10:49:24 +0100

    update locatie

--- src/testen/Unit/UnitGebruikerToevoegen.test.jsx
+++ src/testen/Unit/UnitGebruikerToevoegen.test.jsx
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import GebruikerToevoegen from "../pages/GebruikerToevoegen";
+import GebruikerToevoegen from "../../pages/GebruikerToevoegen";
 
 // Mock navigate van React Router
 const mockNavigate = vi.fn();
